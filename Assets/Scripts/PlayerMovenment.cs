b5dc68e4ff61a2df472e0147a6109d18fa100854
sebas777 <sebastiancast06@hotmail.com>
2025-03-07 11:34:46 -0500

    [sc]feat:sprite turns

--- Assets/Scripts/PlayerMovenment.cs
+++ Assets/Scripts/PlayerMovenment.cs
@@ -7,14 +7,18 @@ public class PlayerMovenment : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 input;
+    private bool isFacingRight = true;
        void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    /// <summary>
+    /// Process inputs and flip player if necessary
     void Update()
     {
         ProcessInputs();
+        Flip();
     }
 
     private void ProcessInputs()
@@ -27,5 +31,18 @@ public class PlayerMovenment : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = input * speed;
+    } 
+
+    private void Flip()
+    {
+        // si esta mirando a la derecha  y aprieto a la izquierda
+        // si esta mirando a la izquierda y aprieto a la derecha
+        if(isFacingRight && input.x < 0f || !isFacingRight && input.x > 0f)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+            isFacingRight = !isFacingRight; 
+        }
     }
 }
